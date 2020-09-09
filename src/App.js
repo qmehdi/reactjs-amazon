96059122972b49159778c49056af3b8634ba8811
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-09 18:07:51 -0500

    Completed Header Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     //BEM Convention
     <div className="app">
-        <h1>Hello Clever, Let's build!</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Home */}
     </div>
   );
 }
