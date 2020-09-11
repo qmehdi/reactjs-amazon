dc895e9e13347236bfda77bbcf8a63b3bfdd9958
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-11 02:39:10 -0500

    Able to remove items from Basket

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,18 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    //remove item from basket
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
@@ -20,7 +31,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p>🌟</p>
             ))}
         </div>
-        <button>Remove from Basket</button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -18,6 +18,28 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item],
       };
 
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      // Copy the current basket's contents
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        // Cut out the element to remove from the new Basket
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in basket!`
+        );
+      }
+
+      return {
+        ...state,
+        basket: newBasket,
+      };
+
     default:
       return state;
   }
