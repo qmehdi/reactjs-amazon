59ce14cb6e78ef5cbca6eaec2b7316c192dd07d3
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-10 20:44:55 -0500

    React Context API - Clicking Add-To-Basket button updates cart in Header

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,12 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  //Grab the basket
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -39,7 +43,9 @@ function Header() {
       <Link to="/checkout">
         <div className="header__optionBasket">
           <ShoppingBasketIcon />
-          <span className="header__optionLineTwo header__basketCount">0</span>
+          <span className="header__optionLineTwo header__basketCount">
+            {basket?.length}
+          </span>
         </div>
       </Link>
     </div>
