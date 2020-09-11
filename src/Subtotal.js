61f9f02a56e5346c455b58ff06fcd2c0c9882fe2
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-10 23:13:55 -0500

    Subtotal items count and total $ working

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,13 @@
 import React from "react";
 import CurrencyFormat from "react-currency-format";
 import "./Subtotal.css";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
+  // Grab the basket
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -10,7 +15,7 @@ function Subtotal() {
           <>
             <p>
               {/* Part of the homework */}
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -18,7 +23,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0} // Part of the homework
+        value={getBasketTotal(basket)} // Part of the homework
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,11 @@ export const initialState = {
   basket: [],
 };
 
+// Selector
+// reduce iterates through basket and tallies up the total and returns the value
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
   console.log(action);
 
