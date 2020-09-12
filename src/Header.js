2f6283f7b318c8d6b8afa87a41565a44a925a18e
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-12 03:04:29 -0500

    Able to click Sign Out without refreshing and display Signed In user.email

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,18 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
   //Grab the basket
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      // If you clicked on the header__option div when the text says Sign Out (user is not empty), invoke firebase's signOut method on the auth object and sign out of firebase
+      auth.signOut();
+    }
+  };
 
   return (
     <div className="header">
@@ -25,31 +33,39 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <Link to="/login">
-          <div className="header__option">
-            <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
+        {/* Only if there is no user, only then do we redirect to the login page. If no user then link gets ignored and doesn't get executed */}
+        <Link to={!user && "/login"}>
+          {/* Make this div clickable */}
+          <div onClick={handleAuthentication} className="header__option">
+            <span className="header__optionLineOne">
+              Hello {user ? user.email : "Guest"}
+            </span>
+
+            {/* If user is signed in, display Sign Out, otherwise display Sign In */}
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
           </div>
         </Link>
 
-      <div className="header__option">
-        <span className="header__optionLineOne">Returns</span>
-        <span className="header__optionLineTwo">& Orders</span>
-      </div>
-
-      <div className="header__option">
-        <span className="header__optionLineOne">Your</span>
-        <span className="header__optionLineTwo">Prime</span>
-      </div>
-
-      <Link to="/checkout">
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header__optionLineTwo header__basketCount">
-            {basket?.length}
-          </span>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
         </div>
-      </Link>
+
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
