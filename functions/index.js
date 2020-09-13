9af0f8855d9b8e3d852bd99e14307f8e57e6aa49
Qamber Mehdi <qmehdi110@gmail.com>
2020-09-13 03:38:57 -0500

    Added firebase functions and express boilerplate

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51HPwD1LVy8I7ci0nUC2qEXSIpPWBjfg9Ztn8h2T2YlJJXYiQ76EutycoFuAag49ZlTumDrY0kcy2uXZSXVqieP4z008rL4zZT0"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//  API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API Routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+// firebase emulators:start
+
+// Example endpoint
+// http://localhost:5001/clone-2241f/us-central1/api
