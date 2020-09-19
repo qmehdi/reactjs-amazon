6a3bca12dc6189056bcb5c256222d8c11b18cec3
Qamber Mehdi <qmehdi@Qambers-MBP.attlocal.net>
2020-09-19 02:31:01 -0500

    Successfully able to fetch orders from firebase and render them on /orders page

--- src/Orders.js
+++ src/Orders.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Orders.css";
+import { db } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import Order from "./Order";
 
 function Orders() {
+    const [{ basket, user }, dispatch] = useStateValue();
+    // This orders state is responsible for storing orders in state
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        // If a user is logged in...
+        if (user) {
+            // Reach into firebase to get signed in user's collection of orders
+            db
+            .collection('users')
+            .doc(user?.uid)
+            .collection('orders')
+            .orderBy('created', 'desc')
+            // onSnapshot is the real time value of the database 
+            .onSnapshot(snapshot => (
+                setOrders(snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                })))
+            ))
+        } else {
+            setOrders([])
+        }
+    }, [user])
+     
     return (
         <div className="orders">
             <h1>Your orders</h1>
+
+            <div className="orders__order">
+                {orders?.map(order => (
+                    <Order order={order} />
+                ))}
+            </div>
         </div>
     )
 }
